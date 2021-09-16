d5147b6653368f8e27d693348856ccf04dabff79
Kavya M <76911063+kavya371@users.noreply.github.com>
2021-09-16 09:45:22 +0530

    Revert "useContext"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React from 'react'
-import './App.css'
-import MovieList from './MovieList';
-import Nav from './Nav';
-import   MovieProvider  from './MovieContext.js';
-import AddMovie from './AddMovie';
+import React, { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-todo":
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1
+      };
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed } : t
+        ),
+        todoCount: state.todoCount
+      };
+    default:
+      return state;
+  }
+}
 
 const App = () => {
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0
+  });
+  const [text, setText] = useState();
+
   return (
-    <MovieProvider>
-      <div>
-      <Nav/>
-      <AddMovie/>
-     <MovieList/>
-     </div> 
-    </MovieProvider>
-  )
-}
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch({ type: "add-todo", text });
+          setText("");
+        }}
+      >
+        <input value={text} onChange={e => setText(e.target.value)} />
+      </form>
+      <div>number of todos: {todoCount}</div>
+      {todos.map((t, idx) => (
+        <div
+          key={t.text}
+          onClick={() => dispatch({ type: "toggle-todo", idx })}
+          style={{
+            textDecoration: t.completed ? "line-through" : ""
+          }}
+        >
+          {t.text}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
